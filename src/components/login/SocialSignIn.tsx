63793757a0bf41a64c271d885948c4a37f3c8b3d
seungbeom <ghfk100@naver.com>
2022-07-18 16:51:50 +0900

    [Test] kakao 로그인 테스트

--- src/components/login/SocialSignIn.tsx
+++ src/components/login/SocialSignIn.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as LoginButton } from 'src/assets/icons/socialLogin/logi
 function SocialSignIn() {
   const moveToSocialKakao = () => {
     window.location.replace(
-      'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=968fe442549959a4ab2bb530f508c889&redirect_uri=https://stupy.shop:3000/main/',
+      'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=968fe442549959a4ab2bb530f508c889&redirect_uri=https://stupy.shop:3000/main',
     );
   };
   return (
