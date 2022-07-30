d1e628eac200f22e886c2f699fee33fb7797f606
seungbeom <ghfk100@naver.com>
2022-07-31 04:55:53 +0900

    [Test]

--- src/screen/WebCamscreen.tsx
+++ src/screen/WebCamscreen.tsx
@@ -2,22 +2,24 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-const */
 /* eslint-disable react/button-has-type */
+import { DefaultEventsMap } from '@socket.io/component-emitter';
+import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { useParams, useNavigate } from 'react-router-dom';
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import { enterRoomApi } from 'src/api/webcam';
 import Webcamchatting from '../components/webcamchatting';
 
+let socket: Socket<DefaultEventsMap, DefaultEventsMap> | null = null;
 export default function WebCamscreen() {
   const param = useParams();
   const paramid = param.id;
-  let socket = null;
-  const nav = useNavigate();
 
+  const nav = useNavigate();
+  const localToken = localStorage.getItem('token');
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data } = useQuery('enterRoom', () => enterRoomApi(paramid), {
-    onSuccess: () => {
-      console.log('joinRoomSuccess');
-    },
     onError: () => {
       alert('비정상 접근입니다.');
       nav(-1);
@@ -25,16 +27,18 @@ export default function WebCamscreen() {
   });
   // 소켓연결
 
-  const localToken = localStorage.getItem('token');
-  if (data?.data === true) {
-    socket = io('http://stupy.shop', {
-      // socket = io('http://localhost:3001', {
-      auth: {
-        token: localToken,
-        roomId: paramid,
-      },
-    });
-  }
+  useEffect(() => {
+    if (data) {
+      socket = io('http://stupy.shop', {
+        // socket = io('http://localhost:3001', {
+        auth: {
+          token: localToken,
+          roomId: paramid,
+        },
+      });
+    }
+  }, [data]);
+
   if (socket === null) {
     return null;
   }
