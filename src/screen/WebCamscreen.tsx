a5c1dfd1ec962583242325974546847f0629e3f5
seungbeom <ghfk100@naver.com>
2022-07-31 00:22:25 +0900

    [Feat] 룸 입장시 api 호출

--- src/screen/WebCamscreen.tsx
+++ src/screen/WebCamscreen.tsx
@@ -1,13 +1,28 @@
+/* eslint-disable no-console */
 /* eslint-disable react/button-has-type */
-import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { useNavigate, useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
+import { enterRoomApi } from 'src/api/webcam';
 import Webcamchatting from '../components/webcamchatting';
 
 export default function WebCamscreen() {
   const param = useParams();
   const paramid = param.id;
+  const nav = useNavigate();
+
+  const EnterRoomdata = useQuery('roomTitle', () => enterRoomApi(paramid), {
+    onSuccess: () => {
+      console.log('joinRoomSuccess');
+    },
+    onError: (errormessge) => {
+      // eslint-disable-next-line no-unused-expressions, no-sequences, no-alert
+      nav(-1), alert(errormessge);
+    },
+  });
 
   // 소켓연결
+  console.log(EnterRoomdata);
   const localToken = localStorage.getItem('token');
   const socket = io('http://stupy.shop', {
     // const socket = io('http://localhost:3001', {
@@ -16,6 +31,5 @@ export default function WebCamscreen() {
       roomId: paramid,
     },
   });
-
   return <Webcamchatting socket={socket} />;
 }
