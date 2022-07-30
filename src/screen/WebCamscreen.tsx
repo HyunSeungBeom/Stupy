83520244e3f70b5a6eb1a21ea6a7bb8aa1af4478
seungbeom <ghfk100@naver.com>
2022-07-31 04:19:08 +0900

    [Test] socket 연결전 api 연결

--- src/screen/WebCamscreen.tsx
+++ src/screen/WebCamscreen.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable prefer-const */
 /* eslint-disable react/button-has-type */
 import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
 import { enterRoomApi } from 'src/api/webcam';
 import Webcamchatting from '../components/webcamchatting';
@@ -12,7 +12,7 @@ export default function WebCamscreen() {
   const param = useParams();
   const paramid = param.id;
   let socket;
-  // const nav = useNavigate();
+  const nav = useNavigate();
 
   const { data } = useQuery('enterRoom', () => enterRoomApi(paramid), {
     onSuccess: () => {
@@ -37,7 +37,8 @@ export default function WebCamscreen() {
     return null;
   }
   if (!socket) {
-    console.log('소켓이없어');
+    alert('비정상 접근입니다.');
+    nav('/');
     return null;
   }
   return <Webcamchatting socket={socket} />;
