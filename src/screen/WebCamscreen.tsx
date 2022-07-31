a9c972e321453d86945536d9db0fc138f0039f97
seungbeom <ghfk100@naver.com>
2022-07-31 18:19:43 +0900

    [Test] socket 연결 전 api 호출 확인 테스트

--- src/screen/WebCamscreen.tsx
+++ src/screen/WebCamscreen.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-const */
 /* eslint-disable react/button-has-type */
+import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { useParams, useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
@@ -15,12 +16,17 @@ export default function WebCamscreen() {
   const nav = useNavigate();
   const localToken = localStorage.getItem('token');
 
-  const { data } = useQuery('enterRoom', () => enterRoomApi(paramid), {
-    onError: () => {
-      alert('비정상 접근입니다.');
-      nav(-1);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { isSuccess, data } = useQuery(
+    'enterRoom',
+    () => enterRoomApi(paramid),
+    {
+      onError: () => {
+        alert('비정상 접근입니다.');
+        nav(-1);
+      },
     },
-  });
+  );
 
   // useEffect(() => {
   //   if (data?.data === true) {
@@ -33,17 +39,19 @@ export default function WebCamscreen() {
   //     });
   //   }
   // }, [data?.data]);
-
-  if (data?.data === true) {
-    const socket = io('https://stupy.shop', {
-      // socket = io('https://localhost:3001', {
-      auth: {
-        token: localToken,
-        roomId: paramid,
-      },
-    });
-    return <Webcamchatting socket={socket} />;
-  }
+  useEffect(() => {
+    console.log('useEffect 사용됨!');
+    if (isSuccess) {
+      const socket = io('https://stupy.shop', {
+        // socket = io('https://localhost:3001', {
+        auth: {
+          token: localToken,
+          roomId: paramid,
+        },
+      });
+      <Webcamchatting socket={socket} />;
+    }
+  }, [isSuccess]);
 
   return <div>정상 접근인지 확인 Loading...</div>;
 }
