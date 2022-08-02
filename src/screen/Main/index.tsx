71ca02d37215f30aea5281c061c7ca76ac9cfb3a
demian0721 <demian0721@naver.com>
2022-08-03 01:48:51 +0900

    [Fix]카테고리 삭제 로직 변경

--- src/screen/Main/index.tsx
+++ src/screen/Main/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 import { useSearchParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import moment from 'moment';
 import 'moment/locale/ko';
 import BottomBar from 'src/components/BottomBar';
 import TodoList from 'src/screen/Main/TodoList';
-import icoPlus from 'src/assets/icons/main/icoPlus.svg';
+// import icoPlus from 'src/assets/icons/main/icoPlus.svg';
 import styled from 'styled-components';
 import {
   SetBackGround,
@@ -20,7 +20,7 @@ import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
-import { getTodolist, postTodolist } from 'src/api/todolist';
+import { deleteTodolistId, getTodolist, postTodolist } from 'src/api/todolist';
 import { getMyRooms } from 'src/api/myRooms';
 import MyGroup from './MyGroup';
 import EmptyGroup from './EmptyGroup';
@@ -31,6 +31,8 @@ export default function Main() {
   const queryClient = useQueryClient();
   const [swiperIdx, setSwiperIdx] = useState(0);
   const [params] = useSearchParams();
+  const [isDelete, setIsDelete] = useState(false);
+  const [selectedDelCategory, setSelectedDelCategory] = useState('');
   // console.log(params.get('token'));
 
   const { data: myRoomData } = useQuery(['myRoomData'], getMyRooms);
@@ -45,6 +47,18 @@ export default function Main() {
       console.warn(err);
     },
   });
+  const { mutate: deleteCategory } = useMutation(
+    () => deleteTodolistId(selectedDelCategory),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('todolistData');
+        setIsDelete(false);
+      },
+      onError: (err) => {
+        console.warn(err);
+      },
+    },
+  );
 
   useEffect(() => {
     const kakaotoken = params.get('token');
@@ -58,6 +72,18 @@ export default function Main() {
   const handleAddCategory = () => {
     addCategory();
   };
+
+  const delCategoryOn = () => {
+    if (!isDelete) {
+      setIsDelete(true);
+    } else {
+      deleteCategory();
+    }
+  };
+
+  const onSelectCategory = useCallback((e: string) => {
+    setSelectedDelCategory(e);
+  }, []);
 
   return (
     <SetBackGround>
@@ -118,18 +144,28 @@ export default function Main() {
         <Divider />
         <TitleContainer>
           <Title>TO DO LIST</Title>
-          <AddButton onClick={handleAddCategory}>
-            카테고리 추가
-            <PlusIcon src={icoPlus} alt="" />
-          </AddButton>
+          <div style={{ display: 'flex', gap: 8 }}>
+            <DelButton
+              onClick={delCategoryOn}
+              style={{ color: isDelete ? PRIMARY : '#8f8f8f' }}
+            >
+              삭제
+            </DelButton>
+            {!isDelete && (
+              <AddButton onClick={handleAddCategory}>추가</AddButton>
+            )}
+          </div>
         </TitleContainer>
         {todolistData?.map((item) => {
           return (
             <TodoList
               key={item.todoListId}
               id={item.todoListId}
+              selectedId={selectedDelCategory}
               subject={item.title}
               item={item.todos}
+              isDelete={isDelete}
+              onSelectedCategory={onSelectCategory}
             />
           );
         })}
@@ -161,12 +197,26 @@ const AddButton = styled.div`
   font-size: 16px;
   color: ${PRIMARY};
   font-weight: 500;
+  border: 1px solid #e8e8e8;
+  border-radius: 5px;
+  padding: 5px 11px;
   cursor: pointer;
 `;
-const PlusIcon = styled.img`
-  width: 24px;
-  height: 24px;
+const DelButton = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  color: #8f8f8f;
+  font-weight: 500;
+  border: 1px solid #e8e8e8;
+  border-radius: 5px;
+  padding: 5px 11px;
+  cursor: pointer;
 `;
+// const PlusIcon = styled.img`
+//   width: 24px;
+//   height: 24px;
+// `;
 const SwiperDotContainer = styled.div`
   display: flex;
   justify-content: center;
